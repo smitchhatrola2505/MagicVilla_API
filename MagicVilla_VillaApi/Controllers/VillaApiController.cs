a4ee4a28834dacbb8c84f8099b6a369f734af487
smitchhatrola2505 <schhatrola779@rku.ac.in>
2023-01-18 18:12:21 +0530

    18/01/2023

---
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+	[Route("api/VillaApi")]
+	[ApiController]
+	public class VillaApiController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDTO> GetVillas()
+		{
+			return new List<VillaDTO>()
+			{
+				new VillaDTO{Id=1,Name="Pool View"},
+				new VillaDTO{Id=2,Name="Beach View"} 
+			};
+		}
+	}
+}
