0dbb0d0aa3b98160f481d087cbc0cbb112ce4f2b
smitchhatrola2505 <schhatrola779@rku.ac.in>
2023-01-19 18:14:06 +0530

    19/01/2023

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -1,5 +1,8 @@
-﻿using MagicVilla_VillaApi.Models;
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
 using MagicVilla_VillaApi.Models.Dto;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
@@ -9,13 +12,125 @@ namespace MagicVilla_VillaApi.Controllers
 	public class VillaApiController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<VillaDTO> GetVillas()
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public ActionResult<IEnumerable<VillaDTO>> GetVillas()
 		{
-			return new List<VillaDTO>()
+			return Ok(VillaStore.villaList);
+		}
+
+		[HttpGet("{id:int}", Name = "GetVilla")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ActionResult<VillaDTO> GetVilla(int id)
+		{
+			if (id == 0)
 			{
-				new VillaDTO{Id=1,Name="Pool View"},
-				new VillaDTO{Id=2,Name="Beach View"} 
-			};
+				return BadRequest();
+			}
+
+			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+			if (villa == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(villa);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+			{
+				ModelState.AddModelError("CustomError", "Villa already Exits!");
+				return BadRequest(ModelState);
+			}
+
+			if (villaDTO == null)
+			{
+				return BadRequest(villaDTO);
+			}
+			if (villaDTO.Id > 0)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+			villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+			VillaStore.villaList.Add(villaDTO);
+
+			return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+		}
+
+
+		[HttpDelete("{id:int}", Name = "DeleteVilla")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public IActionResult DeleteVilla(int id)
+		{
+			if (id == 0)
+			{
+				return BadRequest();
+			}
+			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+			if (villa == null)
+			{
+				return NotFound();
+			}
+			VillaStore.villaList.Remove(villa);
+
+			return NoContent();
+		}
+
+		[HttpPut("{id:int}", Name = "UpdateVilla")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+		{
+			if (villaDTO == null || id != villaDTO.Id)
+			{
+				return BadRequest();
+			}
+			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			villa.Name = villaDTO.Name;
+			villa.Sqft = villaDTO.Sqft;
+			villa.Occupancy = villaDTO.Occupancy;	
+
+			return NoContent();
+		}
+
+		[HttpPatch("{id:int}", Name = "UpdateVilla")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+		public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+		{
+			if(patchDTO == null || id == 0)
+			{
+				return BadRequest();
+			}
+			var villa = VillaStore.villaList.FirstOrDefault(u=>u.Id == id);
+
+			if(villa == null)
+			{
+				return BadRequest();
+			}
+			patchDTO.ApplyTo(villa, ModelState);
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			return NoContent();	
 		}
 	}
 }
